137024deeb54923bd3ad96f8ce51befddd671ed4
Petr & Oscar <petr+oscar@appacademy.io>
2015-08-07 17:44:13 -0400

    outline tests for initialize and calculate_match for hand class

---
+++ spec/hand_spec.rb
@@ -0,0 +1,24 @@
+require 'hand'
+require 'rspec'
+
+describe Hand do
+
+  describe "#initialize" do
+    it "initializes with no cards"
+  end
+
+  describe "#receive_cards"
+  describe "#calculate_match" do
+    it "finds royal flush"
+    it "finds straight flush"
+    it "finds four of a kind"
+    it "finds full house"
+    it "finds flush (excluding royal and straight flush)"
+    it "finds straight (excluding royal and straight flush)"
+    it "finds three of a kind"
+    it "finds two pair"
+    it "finds one pair"
+    it "finds high card"
+  end
+
+end
