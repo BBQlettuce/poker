497d139f58c35f693cf2662a0b45ff3211dc0120
Oscar Liu <oscar.liu.13q@gmail.com>
2015-08-09 02:49:20 -0400

    add expect statements for testing #beats?

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -122,55 +122,93 @@ describe Hand do
     let(:two_pair) { Hand.new(
       [Card.new(:hearts, :ace),
         Card.new(:spades, :ace),
-        Card.new(:hearts, :jack,
+        Card.new(:hearts, :jack),
         Card.new(:clubs, :jack),
         Card.new(:hearts, :ten)]) }
     let(:weaker_two_pair) { Hand.new(
       [Card.new(:hearts, :three),
         Card.new(:spades, :three),
-        Card.new(:hearts, :jack,
+        Card.new(:hearts, :jack),
         Card.new(:clubs, :jack),
         Card.new(:hearts, :ten)]) }
     let(:one_pair) { Hand.new(
       [Card.new(:hearts, :ace),
-        Card.new(:spades :ace),
+        Card.new(:spades, :ace),
         Card.new(:hearts, :queen),
         Card.new(:hearts, :jack),
         Card.new(:hearts, :ten)]) }
     let(:weaker_one_pair) { Hand.new(
       [Card.new(:hearts, :seven),
-        Card.new(:spades :seven),
+        Card.new(:spades, :seven),
         Card.new(:hearts, :queen),
         Card.new(:hearts, :jack),
         Card.new(:hearts, :ten)]) }
-    # let(:high_card) { Hand.new(
-    #   [Card.new(:hearts, :ace),
-    #     Card.new(:spades, :king),
-    #     Card.new(:clubs, :queen),
-    #     Card.new(:diamonds, :eight),
-    #     Card.new(:hearts, :three)]) }
+    let(:junk) { Hand.new(
+      [Card.new(:hearts, :ace),
+        Card.new(:spades, :king),
+        Card.new(:clubs, :queen),
+        Card.new(:diamonds, :eight),
+        Card.new(:hearts, :three)]) }
+    let(:weaker_junk) { Hand.new(
+      [Card.new(:hearts, :ace),
+        Card.new(:spades, :king),
+        Card.new(:clubs, :four),
+        Card.new(:diamonds, :eight),
+        Card.new(:hearts, :three)]) }
 
     describe "#identify_combo" do
       it "identifies royal flush" do
         expect(royal_flush.identify_combo).to eq("royal flush")
       end
-      it "identifies straight flush (excluding royal flush)"
+      it "identifies straight flush (excluding royal flush)" do
         expect(straight_flush.identify_combo).to eq("straight flush")
-      it "identifies four of a kind"
+      end
+      it "identifies four of a kind" do
         expect(four_of_a_kind.identify_combo).to eq("four of a kind")
-      it "identifies full house"
+      end
+      it "identifies full house" do
         expect(full_house.identify_combo).to eq("full_house")
-      it "identifies flush (excluding royal and straight flush)"
+      end
+      it "identifies flush (excluding royal and straight flush)" do
         expect(flush.identify_combo).to eq("flush")
-      it "identifies straight (excluding royal and straight flush)"
+      end
+      it "identifies straight (excluding royal and straight flush)" do
         expect(straight.identify_combo).to eq("straight")
-      it "identifies three of a kind"
+      end
+      it "identifies three of a kind" do
         expect(three_of_a_kind.identify_combo).to eq("three of a kind")
-      it "identifies two pair"
+      end
+      it "identifies two pair" do
         expect(two_pair.identify_combo).to eq("two pair")
-      it "identifies one pair"
+      end
+      it "identifies one pair" do
         expect(one_pair.identify_combo).to eq("one_pair")
+      end
       #it "identifies high card"
+    end
+
+    describe "#beats?" do
+      it "returns true if hand beats other hand" do
+        expect(royal_flush.beats?(straight_flush)).to be true
+        expect(straight_flush.beats?(weaker_straight_flush)).to be true
+        expect(weaker_straight_flush.beats?(four_of_a_kind)).to be true
+        expect(four_of_a_kind.beats?(weaker_four_of_a_kind)).to be true
+        expect(weaker_four_of_a_kind.beats?(full_house)).to be true
+        expect(full_house.beats?(weaker_full_house)).to be true
+        expect(weaker_full_house.beats?(flush)).to be true
+        expect(flush.beats?(weaker_flush)).to be true
+        expect(weaker_flush.beats?(straight)).to be true
+        expect(straight.beats?(weaker_straight)).to be true
+        expect(weaker_straight.beats?(three_of_a_kind)).to be true
+        expect(three_of_a_kind.beats?(weaker_three_of_a_kind)).to be true
+        expect(weaker_three_of_a_kind.beats?(two_pair)).to be true
+        expect(two_pair.beats?(weaker_two_pair)).to be true
+        expect(weaker_two_pair.beats?(one_pair)).to be true
+        expect(one_pair.beats?(weaker_one_pair)).to be true
+        expect(weaker_one_pair.beats?(junk)).to be true
+        expect(junk.beats?(weaker_junk)).to be true
+      end
+
     end
 
   end
